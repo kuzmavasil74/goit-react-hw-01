3be055660a091ea9ce92a8cc7e1a8f05b35ae42a
kuzmavasil74 <98240149+kuzmavasil74@users.noreply.github.com>
2024-03-14 13:05:54 +0100

    use clsx task-2

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,11 +1,17 @@
+import clsx from 'clsx'
 import css from './FriendListItem.module.css'
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <div>
-      <img className={css.friendAvatar} src={avatar} alt="Avatar" width="48" />
-      <p className={css.friendName}>{name}</p>
-      <p className={isOnline ? css.online : css.offline}>
+      <img
+        className={clsx(css.friendAvatar)}
+        src={avatar}
+        alt="Avatar"
+        width="48"
+      />
+      <p className={clsx(css.friendName)}>{name}</p>
+      <p className={clsx({ [css.online]: isOnline, [css.offline]: !isOnline })}>
         {isOnline ? 'Online' : 'Offline'}
       </p>
     </div>
